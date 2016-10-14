11d56b776626999663de6908bde66cf130508127
andermatt64 <andermatt64@outlook.com>
2016-10-13 23:10:08 -0400

    Including compiled JavaScript for path lookup

---
+++ dist/simple-fmc-gefs-plugin.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name simple-fmc-gefs-plugin
+// @namespace andermatt64-gefs-plugins
+// @version 0.0.1
+// @description Simple FMC GEFS Plugin
+// @author andermatt64
+// @match http://*.gefs-online.com/gefs.php
+// @grant none
+// ==/UserScript==
+

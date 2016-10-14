edc4dcd508ef46abb1005b16d998f6a5ebf084c1
andermatt64 <andermatt64@outlook.com>
2016-10-13 22:57:22 -0400

    Inital setup of development environment and work flow

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+  var BANNER = '// ==UserScript==\n' +
+               '// @name <%= pkg.name %>\n' +
+               '// @namespace andermatt64-gefs-plugins\n' +
+               '// @version <%= pkg.version %>\n' +
+               '// @description <%= pkg.description %>\n' +
+               '// @author andermatt64\n' +
+               '// @match http://*.gefs-online.com/gefs.php\n' +
+               '// @grant none\n' +
+               '// ==/UserScript==\n\n';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        banner: BANNER,
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: BANNER
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# simple-fmc-gefs-plugin
-Simple FMC for GEFS Online
+# Simple Flight Management Computer Plugin for GEFS Online
+## Development Environment
+ 1. To set up the development environment, make sure [node.js](https://nodejs.org) is installed with [npm](https://www.npmjs.com/).
+ 2. Run `npm install` to install all the dependencies.
+ 3. To build, run `grunt`.

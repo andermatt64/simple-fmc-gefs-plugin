e835d76c7a0c29bef9f6df749e78fd5cb9cc148b
andermatt64 <andermatt64@outlook.com>
2016-10-16 20:54:53 -0400

    Autopilot changes

--- src/autopilot.js
+++ src/autopilot.js
@@ -4,16 +4,24 @@
 
 var APS = {
   // Valid modes:
-  //   HOLD  -> holding on a specific altitude/heading/speed
-  //   ROUTE -> follows a established route
-  //   HLDPT -> holding pattern
-  mode: 'HOLD',
+  //   HDG -> holding on a specific altitude/heading/speed
+  //   RTE -> follows a established route
+  //   HPT -> holding pattern
+  mode: 'HDG',
 
   content: null,
 
   init: function (content) {
     APS.content = content;
 
+    // https://jsfiddle.net/7zthavz0/7/
 
+    var makeModeCell = function () {
+
+    };
+
+    var makeThirdsCell = function () {
+
+    };
   }
 };
